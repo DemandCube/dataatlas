0f3f27b74c4cab2de417769a7cd51fb59a70b4f0
Sam Stites <stites.sr@gmail.com>
2014-04-18 23:54:45 -0700

    [WIP] a simple java get route

--- src/main/java/kafkasphere/GregorServer.java
+++ src/main/java/kafkasphere/GregorServer.java
@@ -1,10 +1,13 @@
 package kafkasphere;
 import static spark.Spark.*;
 import spark.*;
+import kafka.consumer.*;
+import java.util.Properties;
+
 
 
 /**
- * A RESTful server built on the Spark Framework (sparkjava.com).
+ * A RESTful server built on    the Spark Framework (sparkjava.com).
  **/
 public class GregorServer {
     public static void main(String[] args) {
@@ -12,6 +15,14 @@ public class GregorServer {
             @Override
             public Object handle(Request request, Response response) {
                 return "As Gregor Samsa awoke one morning from uneasy dreams he found himself transformed in his bed into a monstrous vermin...";
+            }
+        });
+        get(new Route("/api/:topic") {
+            @Override
+            public Object handle(Request request, Response response) {
+                String topic = request.params(":topic");
+                Properties props = new Properties();
+                ConsumerConfig consumerConfig = new ConsumerConfig();
             }
         });
     }
