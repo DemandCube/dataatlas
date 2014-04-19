35c348947c26641e915b7ad7d62faa012fc85a49
Sam Stites <stites.sr@gmail.com>
2014-04-18 17:05:40 -0700

    hello world!

---
+++ src/main/java/kafkasphere/NachtsServer.java
@@ -0,0 +1,10 @@
+package kafkasphere;
+
+/**
+ * A RESTful server built on the Spark Framework (sparkjava.com).
+ **/
+public class NachtsServer {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
