286be2dc1a4655e3344158daed781c71c1c6157b
Sam Stites <stites.sr@gmail.com>
2014-05-13 20:11:05 -0700

    using controller files

--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -10,23 +10,19 @@ angular
   .state('consumers', {
     url: "/consumers",
     templateUrl: "partials/consumers/consumers.html"
+    controller: "js/features/consumers/controllers.js",
   })
   .state('consumers.list', {
     url: "/list",
     templateUrl: "partials/consumers/consumers.list.html",
-    controller: function($scope) {
-      $scope.consumers = ["A", "List", "Of", "Consumers"];
-    }
   })
   .state('topics', {
     url: "/topics",
+    controller: "js/features/topics/controllers.js",
     templateUrl: "partials/topics/topics.html",
   })
   .state('topics.list', {
     url: "/list",
     templateUrl: "partials/topics/topics.list.html",
-    controller: function($scope) {
-      $scope.topics = ["A", "Set", "Of", "Topics"];
-    }
   })
 });
