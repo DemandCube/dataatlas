07c4dea65e546d36a331a2c730fcbe4d58e3441f
sam@stites.io <stites.sr@gmail.com>
2014-07-08 18:54:43 -0400

    refactor - consistent indentation

--- src/main/java/com/DemandCube/KafkaSphere/kafka/metrics.java
+++ src/main/java/com/DemandCube/KafkaSphere/kafka/metrics.java
@@ -9,33 +9,33 @@ import java.util.List;
 
 
 public class metrics {
-    public static void main(String[] args){
-      List<kafka.javaapi.TopicMetadata> data = MetaDataDump();
-      for (kafka.javaapi.TopicMetadata item : data){
-        System.out.println("topic: " + item.topic());
-        for (kafka.javaapi.PartitionMetadata part: item.partitionsMetadata()){
-          String replicas = "";
-          String isr = "";
-          for (kafka.cluster.Broker replica: part.replicas()){
-            replicas += " " + replica.host();
-          }
-          for (kafka.cluster.Broker replica: part.isr()){
-            isr += " " + replica.host();
-          }
-          System.out.println("    Partition: " + part.partitionId());
-          System.out.println("    Leader   : " + part.leader().host());
-          System.out.println("    Replicas : [" + replicas + "]");
-          System.out.println("    ISR      : [" + isr + "]");
+  public static void main(String[] args){
+    List<kafka.javaapi.TopicMetadata> data = MetaDataDump();
+    for (kafka.javaapi.TopicMetadata item : data){
+      System.out.println("topic: " + item.topic());
+      for (kafka.javaapi.PartitionMetadata part: item.partitionsMetadata()){
+        String replicas = "";
+        String isr = "";
+        for (kafka.cluster.Broker replica: part.replicas()){
+          replicas += " " + replica.host();
         }
+        for (kafka.cluster.Broker replica: part.isr()){
+          isr += " " + replica.host();
+        }
+        System.out.println("    Partition: " + part.partitionId());
+        System.out.println("    Leader   : " + part.leader().host());
+        System.out.println("    Replicas : [" + replicas + "]");
+        System.out.println("    ISR      : [" + isr + "]");
       }
     }
+  }
 
-    public static List<kafka.javaapi.TopicMetadata> MetaDataDump(){
-      kafka.javaapi.consumer.SimpleConsumer consumer = new SimpleConsumer("0.0.0.0", 9092, 100000, 64 * 1024, "metadata");
-      List<String> topics2 = new ArrayList<String>();
-      TopicMetadataRequest req = new TopicMetadataRequest(topics2);
-      kafka.javaapi.TopicMetadataResponse res = consumer.send(req);
-      List<kafka.javaapi.TopicMetadata> data3 = res.topicsMetadata();
-      return data3;
-    };
+  public static List<kafka.javaapi.TopicMetadata> MetaDataDump(){
+    kafka.javaapi.consumer.SimpleConsumer consumer = new SimpleConsumer("0.0.0.0", 9092, 100000, 64 * 1024, "metadata");
+    List<String> topics2 = new ArrayList<String>();
+    TopicMetadataRequest req = new TopicMetadataRequest(topics2);
+    kafka.javaapi.TopicMetadataResponse res = consumer.send(req);
+    List<kafka.javaapi.TopicMetadata> data3 = res.topicsMetadata();
+    return data3;
+  };
 }
