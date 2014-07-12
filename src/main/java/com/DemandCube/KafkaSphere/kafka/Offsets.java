e6037de492e15b8c96b9661ba18cd47cd6422931
sam@stites.io <stites.sr@gmail.com>
2014-07-12 14:57:16 -0400

    init - offsets

---
+++ src/main/java/com/DemandCube/KafkaSphere/kafka/Offsets.java
@@ -0,0 +1,16 @@
+package com.DemandCube.KafkaSphere.kafka;
+
+import com.DemandCube.KafkaSphere.kafka.Metadata;
+
+import kafka.javaapi.TopicMetadata;
+import kafka.javaapi.TopicMetadataRequest;
+import kafka.javaapi.consumer.SimpleConsumer;
+
+import java.util.*;
+
+/**
+ * Created by stites on 7/8/14.
+ */
+public class Offsets {
+
+}
